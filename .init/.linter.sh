2aa086f5239ec1941e0a94110f9a70dca87d3017
Kavia AI <production@kavia.ai>
2025-11-15 11:28:24 +0000

    Checkpoint 1 - cgc312c366

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/sigma-power-mobile-40414-40790/sigma_style_flutter_mobile_frontend
+flutter analyze
+
---
+++ sigma_style_flutter_mobile_frontend/android/app/src/main/kotlin/com/example/sigma_style_flutter_mobile_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sigma_style_flutter_mobile_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
